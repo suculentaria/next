9c36a52ec5157b2d377c92f3c43b05c57b8d4fee
Datavinus <datavinus@gmail.com>
2020-04-28 21:53:02 -0300

    initial commit

---
+++ pages/contato.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Contato = () => {
+  return (
+    <Layout>
+      <h1>Contato</h1>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas porro maiores doloribus dicta et exercitationem illo sequi consectetur velit, eius cumque veniam sapiente consequuntur explicabo facilis, suscipit perspiciatis ipsa dolorem?</p>
+    </Layout>
+  );
+};
+
+export default Contato;
